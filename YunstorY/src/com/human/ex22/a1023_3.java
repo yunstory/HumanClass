9504820f1f8ff9a4b9324d42f10cd8e6f8852239
USER <USER@USER-PC.local>
2020-10-26 08:57:12 +0900

    add

--- YunstorY/src/com/human/ex22/a1023_3.java
+++ YunstorY/src/com/human/ex22/a1023_3.java
@@ -1,5 +1,7 @@
 package com.human.ex22;
 
+import java.util.Arrays;
+
 public class a1023_3 {
 //	public static int sum(int a, int b) {
 //	int sum=0;
@@ -36,15 +38,60 @@ public class a1023_3 {
 	
 	
 	
-	public static void function2(int a) {
-		//리턴값이 없고 매개변수가 있는 함수
-		System.out.println("함수 매개변수 a 값은:"+a);
-		return;//리턴값이 없으면 생략 가능
+//	public static void function2(int a) {
+//		//리턴값이 없고 매개변수가 있는 함수
+//		System.out.println("함수 매개변수 a 값은:"+a);
+//		return;//리턴값이 없으면 생략 가능
+//	}
+//	
+//	public static void main(String[] args) {
+//		function2(10);
+//		function2(12);
+//		
+//	}
+	
+	
+	public static void function1(int a) {
+		a=10;
+	}
+	public static int function2(int a) {
+		a=10;
+		return a;
+	}
+	public static void function3(int a[]) {
+		a[0]=10;
+	}
+	public static int[] function4(int a[]) {
+		a[0]=10;
+		return a;
+
 	}
 	
 	public static void main(String[] args) {
-		function2(10);
-		function2(12);
+		int a=5;
+		int arr[]= {1,2};
+		function1(a);
+		System.out.println(a);
+		
+		
+		int rValue=function2(a);
+		System.out.println(a);
+		System.out.println(rValue);
+		
+		
+		function3(arr);
+		System.out.println(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		
+		int rArr[]=function4(arr);
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(rArr));
+		rArr[1]=9;
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(rArr));
+		
+		
 		
 	}
 	
